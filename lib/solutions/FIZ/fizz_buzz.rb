ac39cf46ec2ef7280edca7edce2da87f6b47483a
sbx_user1051 <sbx_user1051@169.254.12.181>
2019-04-08 13:42:15 +0000

    Mon Apr 08 13:42:15 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -5,11 +5,12 @@ class FizzBuzz
     string = ""
     string += "fizz " if number % 3 == 0 || number.to_s.include?("3")
     string += "buzz " if number % 5 == 0 || number.to_s.include?("5")
-    string += "deluxe" if number % 3 == 0 && number.to_s.include?("3") && number % 2 == 0
-    string += "fake deluxe" if number % 5 == 0 && number.to_s.include?("5") && number % 2 != 0
+    string += "deluxe" if number % 2 == 0 && ((number % 3 == 0 && number.to_s.include?("3")) || (number % 5 == 0 && number.to_s.include?("5")))
+    string += "fake deluxe" if number % 2 != 0 && ((number % 3 == 0 && number.to_s.include?("3")) || (number % 5 == 0 && number.to_s.include?("5")))
     return string == "" ? number.to_s : string.rstrip() 
   end
 
 end
+
 
 

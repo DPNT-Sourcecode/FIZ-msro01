fcc1b8e11d93741636908eed790869168e819ca9
sbx_user1051 <sbx_user1051@169.254.12.181>
2019-04-08 13:18:15 +0000

    Mon Apr 08 13:18:15 UTC 2019

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -55,29 +55,29 @@ describe FizzBuzz do
         expect(f.fizz_buzz(7)).to eq("7")
     end 
 
-    it 'returns "deluxe" if number is even, greater than 10, all the digits are identical and it does not meet the criteria for fizz or buzz' do
+    it 'returns "deluxe" if number is even, divisible by 3 and contains a 3' do
         f = FizzBuzz.new
-        expect(f.fizz_buzz(22)).to eq("deluxe")
-        expect(f.fizz_buzz(44)).to eq("deluxe")
+        expect(f.fizz_buzz(30)).to eq("deluxe")
+        expect(f.fizz_buzz(36)).to eq("deluxe")
     end
 
-    it 'returns "fizz deluxe" if number is even, greater than 10, all digits are identical, is divisible by 3 but not 5 and does not contain a 5' do
-        f = FizzBuzz.new
-        expect(f.fizz_buzz(66)).to eq("fizz deluxe")
-    end
+    # it 'returns "fizz deluxe" if number is even, greater than 10, all digits are identical, is divisible by 3 but not 5 and does not contain a 5' do
+    #     f = FizzBuzz.new
+    #     expect(f.fizz_buzz(66)).to eq("fizz deluxe")
+    # end
 
-    it 'returns "fake deluxe" if number is odd, greater than 10, all the digits are identical and it does not meet the criteria for fizz or buzz' do
-        f = FizzBuzz.new
-        expect(f.fizz_buzz(77)).to eq("fake deluxe")
-    end
+    # it 'returns "fake deluxe" if number is odd, greater than 10, all the digits are identical and it does not meet the criteria for fizz or buzz' do
+    #     f = FizzBuzz.new
+    #     expect(f.fizz_buzz(77)).to eq("fake deluxe")
+    # end
 
-    it 'returns "fizz fake deluxe" if number is odd, greater than 10, all the digits are identical and it contains a 3 but is not divisible by 5' do
-        f = FizzBuzz.new
-        expect(f.fizz_buzz(33)).to eq("fizz fake deluxe")
-    end
+    # it 'returns "fizz fake deluxe" if number is odd, greater than 10, all the digits are identical and it contains a 3 but is not divisible by 5' do
+    #     f = FizzBuzz.new
+    #     expect(f.fizz_buzz(33)).to eq("fizz fake deluxe")
+    # end
 
-    it 'returns "buzz fake deluxe" if number is odd, greater than 10, all the digits are identical and it contains a 5 but is not divisible by 3' do
-        f = FizzBuzz.new
-        expect(f.fizz_buzz(5555)).to eq("buzz fake deluxe")
-    end
+    # it 'returns "buzz fake deluxe" if number is odd, greater than 10, all the digits are identical and it contains a 5 but is not divisible by 3' do
+    #     f = FizzBuzz.new
+    #     expect(f.fizz_buzz(5555)).to eq("buzz fake deluxe")
+    # end
 end

1a2918fe33d1abc34f894bfaec28f4df997ec40f
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:41:02 +0000

    Mon Apr 08 10:41:02 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,7 +2,8 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    return "fizz"
+    return "fizz" if number % 3 == 0
+    return "buzz"
   end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,15 @@
 require './lib/solutions/FIZ/fizz_buzz.rb'
 
 describe FizzBuzz do
-    it 'If the number is a multiple of three then you should write "fizz"' do
+    it 'should return "fizz" if the number is a multiple of three' do
         f = FizzBuzz.new
         expect(f.fizz_buzz(3)).to eq("fizz")
         expect(f.fizz_buzz(6)).to eq("fizz")
     end
+
+    it 'should return "buzz" if the number is a multiple of five' do
+        f = FizzBuzz.new
+        expect(f.fizz_buzz(5)).to eq("buzz")
+        expect(f.fizz_buzz(10)).to eq("buzz")
+    end 
 end

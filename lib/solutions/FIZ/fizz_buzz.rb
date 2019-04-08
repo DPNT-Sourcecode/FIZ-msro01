eee63b76539217fb0450a952b31ad224636e3b61
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:45:53 +0000

    Mon Apr 08 10:45:53 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -5,6 +5,7 @@ class FizzBuzz
     return "fizz buzz" if number % 3 == 0 && number % 5 == 0
     return "fizz" if number % 3 == 0
     return "buzz" if number % 5 == 0
+    return number.to_s 
   end
 
 end

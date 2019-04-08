69dade1e92f2409f6fca6e3710cdb582a1627c21
sbx_user1051 <sbx_user1051@169.254.12.181>
2019-04-08 12:53:55 +0000

    Mon Apr 08 12:53:55 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,8 +4,10 @@ class FizzBuzz
   def fizz_buzz(number)
     string = ""
     string += "fizz " if number % 3 == 0 || number.to_s.include?("3")
-    string += "buzz" if number % 5 == 0 || number.to_s.include?("5")
+    string += "buzz " if number % 5 == 0 || number.to_s.include?("5")
+    string += "deluxe" if number > 10 && number.to_s.split("").uniq.length == 1
     return string == "" ? number.to_s : string.rstrip() 
   end
 
 end
+

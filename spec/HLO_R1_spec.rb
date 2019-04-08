c73b2f81698e653078e074695b6a0a400c2a97df
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 10:23:16 +0000

    Mon Apr 08 10:23:16 UTC 2019

--- spec/HLO_R1_spec.rb
+++ spec/HLO_R1_spec.rb
@@ -2,6 +2,6 @@ require './lib/solutions/HLO/hello.rb'
 describe Hello do
     it "should say hello to the world" do
         h = Hello.new
-        expect(h.hello("")).to equal("Hello world!")
+        expect(h.hello("")).to eq("Hello world!")
     end
 end
